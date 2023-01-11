56900fedff83d0c3619785a7ab1a435cb5c27e55
sladeutley <slade.utley@gmail.com>
2023-01-11 11:15:13 -0800

    applied Layout component to the entirety of our app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react'; 
+
+import { Layout } from '../components'
+
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      {/* Below, 'Component' means whichever component we are currently on */}
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
