9d9694fc78ced2be0db982410983f3254921e29d
sladeutley <slade.utley@gmail.com>
2023-01-11 13:11:36 -0800

    changed company name in nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">U&G</Link>
+        <Link href="/">U&G Market Research</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick="">
