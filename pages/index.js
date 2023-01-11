463671053ba8444753fea6237e5b75ac6fea8999
sladeutley <slade.utley@gmail.com>
2023-01-11 05:40:16 -0800

    got server running, had to comment out some stuff in index.js to get it running bc not all dependencies from npx create next app we're going to use in this version and it was referencing them. Note might have to put the code in process-notes.md step 5 in Head of Layout component later to be able to change the favicon. Also, not sure what the meta stuff does or if it's necessary

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+// import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,6 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <p>Getting Started</p>
+      </main>
+      {/* <main className={styles.main}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -117,7 +120,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
     </>
   )
 }
