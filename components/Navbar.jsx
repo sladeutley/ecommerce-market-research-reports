05fa53a28831d8a88f1046d47f545314f17d1fc9
sladeutley <slade.utley@gmail.com>
2023-01-11 11:22:43 -0800

    coded Navbar. will code shopping cart onClick and make make quantity for it dynamic later

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai' //this is just a shopping icon
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">U&G</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
   )
 }
 
