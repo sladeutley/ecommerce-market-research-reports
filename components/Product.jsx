cd5d4c118dd038ab25c12d6e2bcdce54bf8696a8
sladeutley <slade.utley@gmail.com>
2023-01-11 09:31:10 -0800

    filling out Product.jsx component. After I added code here, I then added a bunch more products to sanity desk

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Product = () => {
+import { urlFor } from '../lib/client' //use this to get the url for the image within our sanity dashboard
+
+const Product = ({ product: { image, name, slug, price } }) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='prduct-card'>
+          <img 
+            src={urlFor(image && image[0])} 
+            // alt="" // I guess we don't need an alt, but could add one to sanity data
+            // Reminder, above '&&' means if there is an image, then give us image[0] or first image
+            width={250}
+            height={250}
+            className="product-image" 
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
