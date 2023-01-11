48544d82b812d2e8ba5d58af14a8ada5f383c88e
sladeutley <slade.utley@gmail.com>
2023-01-11 09:27:44 -0800

    Create product component in index.js to where we pass all of the product data

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,13 @@ const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {/* Above was coded after adding banner item to sanity desk  */}
-        {/* {console.log(bannerData)} */}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
       <FooterBanner />
