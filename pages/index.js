19cc47020fde29d36656446fc3dd4ed8cc05699e
sladeutley <slade.utley@gmail.com>
2023-01-11 07:27:27 -0800

    finished basic homepage/index.js layout and adjusted styles (deleted homepage.css and copied and pasted his css code in globals.css to mine)

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,12 @@ const Home = () => {
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
+
+      <div>
+        {['Product 1', 'Product 2'].map((product) => product)}
+      </div>
+
+      Footer
     </>
   )
 }
