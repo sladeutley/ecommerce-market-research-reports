b11fce7b08a6dc340588b694019f8815de66d406
sladeutley <slade.utley@gmail.com>
2023-01-12 05:55:50 -0800

    coded GetStarted tw biz component and made adjustments to hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from '../styles/style'
-import { discount, robot } from '../assets'
 import GetStarted from "./GetStarted"
 
 const Hero = () => (
@@ -8,7 +7,7 @@ const Hero = () => (
       
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={'./discount.svg'} alt="discount" className="w-[32px] h-[32px]" />
+          <img src={'./Discount.svg'} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">20%</span> Discount For{" "}
             <span className="text-white">1 Month</span> Account
