13335cedd1bbf077a803376c949f4d46aaf1a5af
sladeutley <slade.utley@gmail.com>
2023-01-12 07:52:29 -0800

    implement product details page (this is when user clicks on product it goes to that product page) within new products folder. [slug] is the unique identifier from sanity.io and bc it's in brackets means it's dynamic

---
+++ pages/product/[slug].js
@@ -0,0 +1,10 @@
+import React from 'react'
+//this page is for when user clicks on product it goes to that product page within new products folder. [slug] is the unique identifier from sanity.io and bc it's in brackets means it's dynamic
+
+const [slug] = () => {
+  return (
+    <div>[slug]</div>
+  )
+}
+
+export default [slug]
