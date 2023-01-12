046a86d118a0616f6c29168a237fb0af321e640a
sladeutley <slade.utley@gmail.com>
2023-01-12 07:14:47 -0800

    adjusted get started button to not have margin left on small devices

--- components/GetStarted.jsx
+++ components/GetStarted.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/style';
 
 const GetStarted = () => (
   //Below changed margin left and right - I am not a fan of how it goes to column so soon, but i think it has to bc of the image on right
-  <div className={`${styles.flexCenter} ml-16 mr-40 w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+  <div className={`${styles.flexCenter} sm:ml-16 sm:mr-40 w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         {/* <p className="font-poppins font-medium text-[18px] leading-[23.4px] mr-2"> */}
