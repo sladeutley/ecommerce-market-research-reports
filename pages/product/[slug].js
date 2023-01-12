2e225ff9bc0076ec3bcc12ceb8f12f7583f356b4
sladeutley <slade.utley@gmail.com>
2023-01-12 08:00:20 -0800

    able to render 'Product' on product detail page (when user clicks on product). *This is the beauty of file based routing in next js. we didn't have to implement any library like react-router

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,10 +1,10 @@
 import React from 'react'
 //this page is for when user clicks on product it goes to that product page within new products folder. [slug] is the unique identifier from sanity.io and bc it's in brackets means it's dynamic
 
-const [slug] = () => {
+const ProductDetails = () => {
   return (
-    <div>[slug]</div>
+    <div>Product</div>
   )
 }
 
-export default [slug]
+export default ProductDetails
