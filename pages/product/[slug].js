86484c16b1eb870cf25a49df1acef24eafa5b790
sladeutley <slade.utley@gmail.com>
2023-01-12 08:49:22 -0800

    wan't importing useState which is why I was getting error

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 //this page is for when user clicks on product it goes to that product page within new products folder. [slug] is the unique identifier from sanity.io and bc it's in brackets means it's dynamic
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
